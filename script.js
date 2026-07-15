eda756a763787110a279cdb021d0a6c8dcfd99f6
Boi <51324966+LOL-BOI-LOL@users.noreply.github.com>
2026-07-15 12:38:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -61,11 +61,10 @@ function computeEffects(type1, type2) {
 
 for (let i = 0; i < 18; ++i) {
     typs[i].onclick = () => {
-        if (typs[i].style.color == "#C2BF14") {
+        if (typs[i].style.color == "rgb(194, 191, 20)") {
             typs[i].style.color = "white";
         } else {
-            typs[i].style.color = "#C2BF14";
+            typs[i].style.color = "rgb(194, 191, 20)";
         }
-        typs[i].innerText = typs[i].style.color;
     };
 }
